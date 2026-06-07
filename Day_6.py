5e47ccc03dd629bb32a9fb79bcd77fe3cf96fb7d
AvinashPhadtare <phadtareavinash2008@gmail.com>
2026-06-07 11:56:05 +0530

    DAY 6 of Practice and updated some files content

--- Day_6.py
+++ Day_6.py
@@ -2,43 +2,37 @@ class HistoryTracker:
     def __init__(self):
         self.stack = []
 
-    def visit(self, page:str):
+    def visit(self, page: str):
         self.stack.append(page)
-        return print(f"Visited: {page}")
-    
+        return f"Visited: {page}"
+
     def back(self):
         if not self.stack:
-            return print("No history")
+            return "No history"
         self.stack.pop()
         if not self.stack:
-            return print("No history")
-        last_visited = self.stack[-1]
-        return print(f"Back to {last_visited}")
-    
+            return "No history"
+        return self.stack[-1]
+
     def current(self):
         if not self.stack:
-            return print("No history")
-        current_visit = self.stack[-1]
-        return print(f"Current URL:{current_visit}")
-    
+            return "No history"
+        return self.stack[-1]
+
     def clear(self):
         self.stack.clear()
         print("All history cleared")
 
 
-
-# Example usage:-
-
+# Example usage:
 tracker = HistoryTracker()
-
 
 tracker.visit("/members")
 tracker.visit("/members/1")
 tracker.visit("/member/2")
 
-
-tracker.current()
-tracker.back()
-tracker.current()
+print(tracker.current())
+print(tracker.back())
+print(tracker.current())
 tracker.clear()
-tracker.current()
+print(tracker.current())
