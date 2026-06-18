8b810d2277de769b1144d75faa1d31589987fb4b
AvinashPhadtare <phadtareavinash2008@gmail.com>
2026-06-18 14:08:08 +0530

    Day 15 of practicing Python

--- Day_15.py
+++ Day_15.py
@@ -6,3 +6,11 @@ def first_non_repeating_character(s: str):
         if char_count[ch] == 1:
             return ch
     return -1
+
+
+# Exaple usage:
+print(first_non_repeating_character("abacabad"))  # Output: 'c'
+print(first_non_repeating_character("abacabaabacaba"))  # Output: -1
+print(first_non_repeating_character("z"))  # Output: 'z'
+print(first_non_repeating_character("bcb"))  # Output: 'c'  
+print(first_non_repeating_character("bcccccccb"))  # Output: 'b'
